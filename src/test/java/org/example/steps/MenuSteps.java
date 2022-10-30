9ef5b9e550335f1533c96968608c998b2bbbf09d
Nikolay-4k <Custodian133005@gmail.com>
2022-10-30 19:07:45 +0200

    Cucumber added

---
+++ src/test/java/org/example/steps/MenuSteps.java
@@ -0,0 +1,35 @@
+package org.example.steps;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.example.pages.DocsPage;
+import org.example.pages.HomePage;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MenuSteps {
+    private final HomePage homePage = new HomePage();
+    private final DocsPage docsPage = new DocsPage();
+
+    @When("the user opens Cucumber website")
+    public void openCucumberWebsite() {
+        homePage.openCucumberWebsite();
+    }
+
+    @When("the user clicks on the {string} section")
+    public void clickOnSection(String section) {
+        homePage.menuSection(section).click();
+    }
+
+    @When("the user clicks on the {string} item")
+    public void clicksOnItem(String item) {
+        homePage.menuChildItem(item).click();
+    }
+
+    @Then("page with title {string} is displayed")
+    public void pageWithTitleIsDisplayed(String pageTitle) {
+        assertThat(docsPage.isPageWithTitleDisplayed(pageTitle))
+                .withFailMessage("Title is not '{}'", pageTitle)
+                .isTrue();
+    }
+}
